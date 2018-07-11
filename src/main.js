89576f26e511858d1a1f7ad7669c4a3b99717549
G.S. Prayoga <satria.prayoga@gmail.com>
2018-07-11 15:06:38 +0700

    initial commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import '@babel/polyfill'
+import Vue from 'vue'
+import './plugins/vuetify'
+import App from './App.vue'
+import './plugins/main.styl'
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App)
+}).$mount('#app')
